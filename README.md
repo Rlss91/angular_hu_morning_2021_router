a51208f81da25547c3a9305037b0fe72fb95c0da
rlss91 <rlss91@gmail.com>
2022-02-08 15:51:29 +0200

    server info

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## home work
+
+1. create component to display server details from server info
+   (child component)
+   (server-info -> child component)
+2. create button to change the status of the selected server
+   (server-info <- child component)
+3. when server status is
+   3.1 true:
+   then in servers route the server button must be green
+   3.2 false:
+   then server button must be red
+   (subject)
--- src/app/pages/server-info/server-info.component.ts
+++ src/app/pages/server-info/server-info.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import ServerData from 'src/app/model/ServerData.model';
+import { ServersDataService } from 'src/app/services/servers-data.service';
 
 @Component({
   selector: 'app-server-info',
   templateUrl: './server-info.component.html',
-  styleUrls: ['./server-info.component.css']
+  styleUrls: ['./server-info.component.css'],
 })
 export class ServerInfoComponent implements OnInit {
-
-  constructor() { }
+  selectedServer: ServerData;
+  constructor(
+    private route: ActivatedRoute,
+    private serversDataService: ServersDataService
+  ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((qparams: Params) => {
+      console.log('qparams from server info', qparams);
+      this.selectedServer = this.serversDataService.serversArr.find(
+        (serverItem) => serverItem.code == qparams['servercode']
+      );
+      console.log('this.selectedServer', this.selectedServer);
+    });
   }
-
 }
