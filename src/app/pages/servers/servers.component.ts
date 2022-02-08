3c079b0884eb6aa864788c8fd617cfa9fcc15f14
rlss91 <rlss91@gmail.com>
2022-02-08 15:10:23 +0200

    d

--- src/app/pages/servers/servers.component.ts
+++ src/app/pages/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import ServerData from 'src/app/model/ServerData.model';
 import { ServersDataService } from 'src/app/services/servers-data.service';
 
@@ -9,11 +10,22 @@ import { ServersDataService } from 'src/app/services/servers-data.service';
 })
 export class ServersComponent implements OnInit {
   serverArr: ServerData[];
-  constructor(private serversDataService: ServersDataService) {
+  constructor(
+    private serversDataService: ServersDataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.serverArr = this.serversDataService.serversArr;
   }
 
   ngOnInit(): void {}
 
-  handleServerClick(chosenServerCode: number): void {}
+  handleServerClick(chosenServerCode: number): void {
+    this.router.navigate(['serverinfo'], {
+      relativeTo: this.route,
+      queryParams: {
+        servercode: chosenServerCode,
+      },
+    });
+  }
 }
