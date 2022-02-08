1755b302c2b68d181ffb372320f059dd3c8ceda9
rlss91 <rlss91@gmail.com>
2022-02-08 14:36:23 +0200

    dynamic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PetsComponent } from './pages/pets/pets.component';
 import { QueryParamsComponent } from './pages/query-params/query-params.component';
+import { ServerInfoComponent } from './pages/server-info/server-info.component';
 import { ServersComponent } from './pages/servers/servers.component';
 
 const routes: Routes = [
@@ -10,7 +11,11 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'pets', component: PetsComponent },
   { path: 'query', component: QueryParamsComponent },
-  { path: 'servers', component: ServersComponent },
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [{ path: 'serverinfo', component: ServerInfoComponent }],
+  },
 ];
 
 @NgModule({
